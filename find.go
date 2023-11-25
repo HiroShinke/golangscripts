e6157af08b16a138890a20ea3d33bb9d9b844811
Your Name <you@example.com>
2023-11-25 18:01:47 +0900

    find

---
+++ find.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+func do_rec_file(path string, proc func(string, fs.FileInfo) error) error {
+
+	info, err := os.Stat(path)
+	err = proc(path, info)
+	if err != nil {
+		return err
+	}
+
+	if info.IsDir() {
+		entries, err := os.ReadDir(path)
+		if err != nil {
+			return err
+		}
+		for _, entry := range entries {
+			path := filepath.Join(path, entry.Name())
+			if err := do_rec_file(path, proc); err != nil {
+				fmt.Println(err)
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func main() {
+
+	ifile := flag.String("i", "", "input file")
+	flag.Parse()
+	fmt.Print("i = ", *ifile, "\n")
+
+	proc := func(path string, info fs.FileInfo) error {
+		fmt.Printf("path = %s\n", path)
+		return nil
+	}
+
+	if err := do_rec_file(*ifile, proc); err != nil {
+		fmt.Println(err)
+	}
+
+}
