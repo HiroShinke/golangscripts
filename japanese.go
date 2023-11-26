ce616259096a4fc312b7c0c519f2bce367bbbadf
Your Name <you@example.com>
2023-11-26 17:11:56 +0900

    japanese

---
+++ japanese.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	const nihongo = "日本語"
+	for index, runeValue := range nihongo {
+		fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
+	}
+
+	for i, w := 0, 0; i < len(nihongo); i += w {
+		runeValue, width := utf8.DecodeRuneInString(nihongo[i:])
+		fmt.Printf("%#U starts at byte position %d\n", runeValue, i)
+		w = width
+	}
+
+}
